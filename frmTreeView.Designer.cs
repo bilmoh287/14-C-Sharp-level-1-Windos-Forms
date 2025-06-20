d000b1ec77c888284b95cbe3cf557d764e6ed1f8
bilmoh287 <142092794+bilmoh287@users.noreply.github.com>
2025-06-21 00:26:52 +0300

    added To Do List Project Using TreeView and Combo Box

---
+++ frmTreeView.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class frmTreeView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode36 = new System.Windows.Forms.TreeNode("Pray");
+            System.Windows.Forms.TreeNode treeNode37 = new System.Windows.Forms.TreeNode("Programming");
+            System.Windows.Forms.TreeNode treeNode38 = new System.Windows.Forms.TreeNode("Zikr");
+            System.Windows.Forms.TreeNode treeNode39 = new System.Windows.Forms.TreeNode("Quran");
+            System.Windows.Forms.TreeNode treeNode40 = new System.Windows.Forms.TreeNode("Work");
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.CheckBoxes = true;
+            this.treeView1.Location = new System.Drawing.Point(22, 59);
+            this.treeView1.Name = "treeView1";
+            treeNode36.Name = "Pray";
+            treeNode36.Text = "Pray";
+            treeNode37.Name = "Programming";
+            treeNode37.Text = "Programming";
+            treeNode38.Name = "Zikr";
+            treeNode38.Text = "Zikr";
+            treeNode39.Name = "Quran";
+            treeNode39.Text = "Quran";
+            treeNode40.Name = "Work";
+            treeNode40.Text = "Work";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode36,
+            treeNode37,
+            treeNode38,
+            treeNode39,
+            treeNode40});
+            this.treeView1.Size = new System.Drawing.Size(317, 286);
+            this.treeView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Ravie", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(30, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(292, 38);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "My To Do List";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(395, 167);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 43);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Add Task";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(586, 167);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(152, 43);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Remove Task";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(481, 216);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(152, 43);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Remove All TAsk";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(390, 111);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(348, 40);
+            this.textBox1.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(571, 321);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // frmTreeView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(864, 516);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "frmTreeView";
+            this.Text = "frmTreeView";
+            this.Load += new System.EventHandler(this.frmTreeView_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
---
+++ frmTreeView.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class frmTreeView : Form
+    {
+        public frmTreeView()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                TreeNode newNode = new TreeNode(textBox1.Text);
+                newNode.Name = textBox1.Text;
+                treeView1.Nodes.Add(newNode);
+
+                comboBox1.Items.Add(textBox1.Text);
+            }
+            else
+            {
+                MessageBox.Show("Please Enter what you want to add first!", 
+                    "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            }
+            textBox1.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                TreeNode[] FoundNodes =  treeView1.Nodes.Find(textBox1.Text, true);
+                if(FoundNodes.Length > 0)
+                {
+                    TreeNode NodeToRemove = FoundNodes[0];
+                    NodeToRemove.Remove();
+
+
+                    MessageBox.Show($"Node '{textBox1.Text}' removed successfully.");
+
+                }
+
+                else
+                {
+                    MessageBox.Show($"Node '{textBox1.Text}' not found.");
+                }
+            }
+
+            else
+            {
+                MessageBox.Show("Please Enter what you want Remove first!",
+                    "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            }
+            textBox1.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            treeView1 .Nodes.Clear();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmTreeView_Load(object sender, EventArgs e)
+        {
+            string treeViewSizeString = treeView1.Nodes.Count.ToString();
+            comboBox1.Items.Clear();
+            comboBox1.Items.Add(treeViewSizeString);
+
+            foreach (TreeNode node in treeView1.Nodes)
+            {
+                comboBox1.Items.Add(node.Text);
+            }
+            comboBox1.SelectedIndex = 0;
+
+        }
+    }
+}
